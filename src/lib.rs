b9193ab694df3d921fd87f74923a12c14383de70
JayanAXHF <sunil.chdry@gmail.com>
2025-02-01 00:17:13 +0530

    interchange

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use decrypt_cookies::{Browser, ChromiumBuilder, FirefoxBuilder};
 
 #[tokio::main]
 pub async fn main() -> miette::Result<()> {
+    dbg!("safasf 0");
     let chromium = FirefoxBuilder::new(Browser::Firefox).build().await?;
     dbg!("safasf 1");
     let all_cookies = chromium.get_cookies_all().await?;
