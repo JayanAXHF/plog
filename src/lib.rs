3cc6036d73ea72156443c12e4502c8d356fb6171
JayanAXHF <sunil.chdry@gmail.com>
2025-02-01 14:49:44 +0530

    fixed

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,14 @@ use decrypt_cookies::{Browser, ChromiumBuilder, FirefoxBuilder};
 #[tokio::main]
 pub async fn main() -> miette::Result<()> {
     dbg!("safasf 0");
-    let chromium = FirefoxBuilder::new(Browser::Firefox).build().await?;
+    let chromium = ChromiumBuilder::new(Browser::Chrome).build().await?;
     dbg!("safasf 1");
     let all_cookies = chromium.get_cookies_all().await?;
     dbg!("safasf");
-    dbg!(&all_cookies[0]);
-
+    dbg!(&all_cookies
+        .iter()
+        .filter(|cookie| cookie.host_key.contains("lv"))
+        .collect::<Vec<_>>());
     let jar: reqwest::cookie::Jar = all_cookies.into_iter().collect();
 
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use portal_logger::main as other_main;
 fn main() {
     println!("Hello, world!");
-    let _ = other_main();
+    other_main().unwrap();
 }
